4b5e939de61129e2367d66ba0145f0a9bd7dfa5f
seo003 <sy030212@naver.com>
2023-09-05 22:46:43 +0900

    design

--- src/main/java/com/example/demo/controller/mainController.java
+++ src/main/java/com/example/demo/controller/mainController.java
@@ -5,11 +5,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-public class mainController {
+public class MainController {
+	
+	//애플리케이션의 기본 URL (예: http://localhost:8080/)에 접근할 때 이 메소드가 실행
 	@RequestMapping(value = "/", method = RequestMethod.GET)
     public String home() {
-		System.out.println("home controller start");
-                   
-        return "home";
+		          
+        return "home"; //뷰 반환(home)
     }
 }
